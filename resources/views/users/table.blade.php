b79e8303ebf0fcc74ee89b2f2f060e6b050e484b
gorretgolden <nabatanzigorret143@gmail.com>
2024-11-16 01:42:33 +0300

    new update

---
+++ resources/views/users/table.blade.php
@@ -0,0 +1,41 @@
+<div class="table-responsive">
+    <table class="table" id="users-table">
+        <thead>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email</th>
+                <th>Phone Number</th>
+               
+                <th>Image</th>
+                <th>Status</th>
+                <th>Phone Number Two</th>
+                <th>Role Id</th>
+                <th colspan="3">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($users as $user)
+            <tr>
+                <td>{{ $user->first_name }}</td>
+                <td>{{ $user->last_name }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->phone_number }}</td>
+                <td>{{ $user->image }}</td>
+                <td>{{ $user->status }}</td>
+                <td>{{ $user->phone_number_two }}</td>
+                <td>{{ $user->role_id }}</td>
+                <td class=" text-center">
+                    {!! Form::open(['route' => ['users.destroy', $user->id], 'method' => 'delete']) !!}
+                    <div class='btn-group'>
+                        <a href="{!! route('users.show', [$user->id]) !!}" class='btn btn-light action-btn '><i class="fa fa-eye"></i></a>
+                        <a href="{!! route('users.edit', [$user->id]) !!}" class='btn btn-warning action-btn edit-btn'><i class="fa fa-edit"></i></a>
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger action-btn delete-btn', 'onclick' => 'return confirm("Are you sure want to delete this record ?")']) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
