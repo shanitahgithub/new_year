50ce93113ab08be14e04834fe7ea5235d2816c18
gorretgolden <nabatanzigorret143@gmail.com>
2024-11-15 23:24:01 +0300

    Updated menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -4,10 +4,10 @@
     </a>
 </li>
 <li class="{{ Request::is('settings*') ? 'active' : '' }}">
-    <a href="{{ route('settings.index') }}"><i class="fa fa-edit"></i><span>@lang('models/settings.plural')</span></a>
+    <a href="{{ route('settings.index') }}"><i class="fa fa-cog"></i><span>@lang('models/settings.plural')</span></a>
 </li>
 
 <li class="side-menus {{ Request::is('users*') ? 'active' : '' }}">
-    <a class="nav-link" href="{{ route('users.index') }}"><i class="fas fa-building"></i><span>Users</span></a>
+    <a class="nav-link" href="{{ route('users.index') }}"><i class="fas fa-users"></i><span>Users</span></a>
 </li>
 
