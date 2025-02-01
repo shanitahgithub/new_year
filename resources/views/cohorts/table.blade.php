24fc7ffe4e5bf4eefab1a4c4dbfb50d417f1e226
shanitahgithub <nakaggshanitah874@gmail.com>
2025-02-01 14:57:53 +0300

    Changes

--- resources/views/cohorts/table.blade.php
+++ resources/views/cohorts/table.blade.php
@@ -1,51 +1,50 @@
-<!-- resources/views/cohorts/table.blade.php -->
-
-<table id="cohorts-table" class="table table-striped table-bordered">
-    <thead>
-        <tr>
-            {{-- <th>ID</th> --}}
-            <th>Name</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>Status</th>
-            <th>Expected Graduation Date</th> <!-- Added Expected Graduation Date -->
-            <th>Curriculum</th> <!-- Added Curriculum -->
-            <th>Number of Students</th> <!-- Added Number of Students -->
-            <th>Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($cohorts as $cohort)
-        <tr>
-            {{-- <td>{{ $cohort->id }}</td> --}}
-            <td>{{ $cohort->name }}</td>
-            <td>{{ \Carbon\Carbon::parse($cohort->start_date)->format('d M Y') }}</td>
-            <td>{{ \Carbon\Carbon::parse($cohort->end_date)->format('d M Y') }}</td>
-            <td>
-                @if ($cohort->status == 'active')
-                <span class="badge badge-success">{{ ucfirst($cohort->status) }}</span>
-                @elseif ($cohort->status == 'inactive')
-                <span class="badge badge-danger">{{ ucfirst($cohort->status) }}</span>
-                @else
-                <span class="badge badge-secondary">{{ ucfirst($cohort->status) }}</span>
-                @endif
-            </td>
-            <td>{{ \Carbon\Carbon::parse($cohort->expected_graduation_date)->format('d M Y') }}</td>
-            <!-- Expected Graduation Date -->
-            <td>{{ $cohort->curriculum }}</td> <!-- Curriculum -->
-            <td>{{ $cohort->number_of_students }}</td> <!-- Number of Students -->
-            <td>
-                <a href="{{ route('cohorts.show', $cohort->id) }}" class="btn btn-info btn-sm">View</a>
-                <a href="{{ route('cohorts.edit', $cohort->id) }}" class="btn btn-sm btn-warning">Edit</a>
-
-                <form action="{{ route('cohorts.destroy', $cohort->id) }}" method="POST" style="display:inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-sm btn-danger"
-                        onclick="return confirm('Are you sure you want to delete this cohort?')">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table" id="cohorts-table">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Start Date</th>
+                <th>End Date</th>
+                <th>Status</th>
+                <th>Expected Graduation Date</th>
+                <th>Curriculum</th>
+                <th>Number of Students</th>
+                <th colspan="3" class="text-center">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($cohorts as $cohort)
+            <tr>
+                <td>{{ $cohort->name }}</td>
+                <td>{{ \Carbon\Carbon::parse($cohort->start_date)->format('d M Y') }}</td>
+                <td>{{ \Carbon\Carbon::parse($cohort->end_date)->format('d M Y') }}</td>
+                <td>
+                    @if ($cohort->status == 'active')
+                    <span class="badge badge-success">{{ ucfirst($cohort->status) }}</span>
+                    @elseif ($cohort->status == 'inactive')
+                    <span class="badge badge-danger">{{ ucfirst($cohort->status) }}</span>
+                    @else
+                    <span class="badge badge-secondary">{{ ucfirst($cohort->status) }}</span>
+                    @endif
+                </td>
+                <td>{{ \Carbon\Carbon::parse($cohort->expected_graduation_date)->format('d M Y') }}</td>
+                <td>{{ $cohort->curriculum }}</td>
+                <td>{{ $cohort->number_of_students }}</td>
+                <td class="text-center">
+                    {!! Form::open(['route' => ['cohorts.destroy', $cohort->id], 'method' => 'delete']) !!}
+                    <div class="btn-group">
+                        <a href="{{ route('cohorts.show', [$cohort->id]) }}" class="btn btn-light action-btn"><i
+                                class="fa fa-eye"></i></a>
+                        <a href="{{ route('cohorts.edit', [$cohort->id]) }}"
+                            class="btn btn-warning action-btn edit-btn"><i class="fa fa-edit"></i></a>
+                        {!! Form::button('<i class="fa fa-trash"></i>', ['type' => 'submit', 'class' => 'btn btn-danger
+                        action-btn delete-btn', 'onclick' => 'return confirm("Are you sure you want to delete this
+                        record?")']) !!}
+                    </div>
+                    {!! Form::close() !!}
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
--- resources/views/student_applications/table.blade.php
+++ resources/views/student_applications/table.blade.php
@@ -1,14 +1,10 @@
-@extends('layouts.app')
+{{-- @extends('layouts.app')
 
 @section('content')
 <div class="container">
     <h1>Student Applications</h1>
 
-    {{-- @if (session('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif --}}
+
 
     <a href="{{ route('student_applications.create') }}" class="btn btn-primary mb-3">Create New Application</a>
 
@@ -57,7 +53,7 @@
 
 
 
-                    {{-- <td>{{ ucfirst($application->status) }}</td> --}}
+
                     <td>{{ $application->program->name }}</td>
                     <td>{{ $application->points_scored }}</td>
                     <td>{{ $application->secondary_school }}</td>
@@ -81,6 +77,91 @@
                             <button type="submit" class="btn btn-danger"
                                 onclick="return confirm('Are you sure you want to delete this application?')">Delete</button>
                         </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>
+@endsection --}}
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h1>Student Applications</h1>
+
+    <a href="{{ route('student_applications.create') }}" class="btn btn-primary mb-3">Create New Application</a>
+
+    <div class="table-responsive">
+        <table class="table table-bordered" id="student-applications-table">
+            <thead>
+                <tr>
+                    {{-- <th>#</th> --}}
+                    <th>User</th>
+                    <th>Date of Birth</th>
+                    <th>Address</th>
+                    <th>Status</th>
+                    <th>Program</th>
+                    <th>Points Scored</th>
+                    <th>Secondary School</th>
+                    <th>Guardian Name</th>
+                    <th>Guardian Contact</th>
+                    <th>Nationality</th>
+                    <th>Interview Date</th>
+                    <th>Interview Result</th>
+                    <th>Submitted Documents</th>
+                    <th>Combination</th>
+                    <th>UACE Year of Completion</th>
+                    <th colspan="3" class="text-center">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($applications as $application)
+                <tr>
+                    {{-- <td>{{ $application->id }}</td> --}}
+                    <td>{{ $application->user->first_name }} {{ $application->user->last_name }}</td>
+                    <td>{{ $application->date_of_birth }}</td>
+                    <td>{{ $application->address }}</td>
+                    <td>
+                        @if ($application->status == 'approved')
+                        <span class="badge badge-success">{{ $application->status }}</span>
+                        @elseif ($application->status == 'rejected')
+                        <span class="badge badge-danger">{{ $application->status }}</span>
+                        @elseif ($application->status == 'pending')
+                        <span class="badge badge-warning">{{ $application->status }}</span>
+                        @else
+                        <span class="badge badge-secondary">{{ $application->status }}</span>
+                        @endif
+                    </td>
+                    <td>{{ $application->program->name }}</td>
+                    <td>{{ $application->points_scored }}</td>
+                    <td>{{ $application->secondary_school }}</td>
+                    <td>{{ $application->guardian_name }}</td>
+                    <td>{{ $application->guardian_contact }}</td>
+                    <td>{{ $application->nationality }}</td>
+                    <td>{{ $application->interview_date }}</td>
+                    <td>{{ ucfirst($application->interview_result) }}</td>
+                    <td>{{ $application->submitted_documents }}</td>
+                    <td>{{ $application->combination }}</td>
+                    <td>{{ $application->uace_year_of_completion }}</td>
+                    <td class="text-center">
+                        <div class="btn-group">
+                            <a href="{{ route('student_applications.show', $application->id) }}"
+                                class="btn btn-light">View</i></a>
+                            <a href="{{ route('student_applications.edit', $application->id) }}"
+                                class="btn btn-warning">Edit</a>
+                            {{-- <a href="{{ route('student_applications.edit', $application->id) }}"
+                                class="btn btn-warning"><i class="fa fa-edit"></i></a> --}}
+                            <form action="{{ route('student_applications.destroy', $application->id) }}" method="POST"
+                                style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger"
+                                    onclick="return confirm('Are you sure you want to delete this application?')">
+                                    Delete</button>
+                            </form>
+                        </div>
                     </td>
                 </tr>
                 @endforeach
